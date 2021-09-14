5fdad661a595fea55389f6331e1806ebcd7ea487
Niyaz <niazlv03@gmail.com>
2021-09-15 00:25:29 +0300

    написал дз на 15/09/2021

---
+++ 15.09.2021Homework/2. уравнение.pas
@@ -0,0 +1,6 @@
+﻿var x: real;
+begin
+  write('введите х: ');
+  readln(x);
+  writeln('y=', (14*x*x)-(3*x)+21);
+end.
---
+++ 15.09.2021Homework/4. Впишется не впишется.pas
@@ -0,0 +1,10 @@
+﻿var r,a,d:real;
+begin
+  write('введите сторону квадрата и радиус круга: ');
+  readln(a,r);
+  d:=sqrt(a*a*2);
+  if((d/2)<= r) then
+    writeln('Квадрат впишется')
+  else
+    writeln('Квадрат не впришется');
+end.
