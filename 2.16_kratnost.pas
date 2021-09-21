7f07cc38d3f2a7c9b076557e3595e4de0a3a7b4d
Niyaz <niazlv03@gmail.com>
2021-09-22 00:46:53 +0300

    2.16-2.17

---
+++ 2.16_kratnost.pas
@@ -0,0 +1,12 @@
+﻿var k,l,m: integer;
+begin
+  write('введите 3 числа через пробел: ');
+  readln(k,l,m);
+  if(k mod l = 0) then writeln(k,' кратно ', l);
+  if(k mod m = 0) then writeln(k,' кратно ', m);
+  if(m mod l = 0) then writeln(m,' кратно ', l);
+ 
+  if(l mod k = 0) then writeln(l,' кратно ', k);
+  if(m mod k = 0) then writeln(m,' кратно ', k);
+  if(l mod m = 0) then writeln(l,' кратно ', m);
+end.
---
+++ 2.17_mod3.pas
@@ -0,0 +1,14 @@
+﻿var a,b,c,d,f:integer;
+begin
+  write('введите трехзначное число: ');
+  readln(a);
+  b:= Trunc(a/100);
+  c:= Trunc((a/10)) mod 10;
+  d:= a mod 10;
+  f:= b+c+d;
+  if(f mod 3 = 0) then
+    writeln('число делится на 3')
+  else
+    writeln('число не делится на 3');
+  //write(a,' ',b,' ',c, ' ',d);
+end.
