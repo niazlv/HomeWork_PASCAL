e8b6ea7439c693f02a4bd40d8ed8dcc49a9dd76f
Niyaz <niazlv03@gmail.com>
2021-10-06 21:13:49 +0300

    ЗА ЧТО МНЕ ЭТИ СТРАДАНИЯ С ПАСКАЛЕМ. ЧТО Я СДЕЛАЛ В ЭТОЙ ЖИЗНИ НЕ ТАК. ЗА КАКОЕ ТАКОЕ ЗЛО МНЕ ЭТОТ ПАСКАЛЬ.(Крик души не спящей вторые сутки изза дедлайнов)

---
+++ 06.10.2021/3.12.pas
@@ -0,0 +1,15 @@
+﻿var a,s:real;
+var i,m,x: integer;
+
+begin
+  write('введите число x: ');
+  readln(x);
+  write('введите число m: ');
+  readln(m);
+  a:=1;
+  for i:=1 to m+1 do
+  begin
+    a:=a*(1+x);
+  end;
+  write('бином ньютона от (1+',x,')^',m,' = ', a);
+end.
---
+++ 06.10.2021/3.46.pas
@@ -0,0 +1,23 @@
+﻿var i,n,a,s,ss,x:real;
+begin
+  write('введите число n и x: ');
+  readln(n,x);
+  s:=0;
+  i:=0;
+  while(i<=n) do
+  begin
+    ss:=1;
+    a:=0;
+    if(a<i)then
+    while(a<i) do
+    begin
+      ss:=ss*sin(x);
+      a:=a+1;
+    end
+    else
+      ss:=0;
+    s:=s+ss;
+    i:=i+1;
+  end;
+  write(s);
+end.
