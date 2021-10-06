53fbf12899a2f574f1a4cb1b39e5d3b0ef601873
Niyaz notebook <niazlv03@gmail.com>
2021-10-06 10:29:56 +0300

    добавил 2 задачи

---
+++ 06.10.2021/3.11.pas
@@ -0,0 +1,11 @@
+﻿var a,b,c:real;
+
+begin
+  a:=-1;
+  repeat
+  if(a>0) then
+    writeln('Кол-во миль: ',a*1.609344);
+  write('введите мили(чтобы завершить введите отрицательное число: ');
+  readln(a);
+  until a <0;
+end.
---
+++ 06.10.2021/3.27.pas
@@ -0,0 +1,19 @@
+﻿var
+    n, i: integer;
+    x, sum, res_f: real;
+begin
+    write('Введите аргумент x, где x < 1: ');
+    readln(x);
+    
+    res_f := ln(1-x);
+    sum := x;
+    i := 2;
+    n := 0;
+    
+    while abs(res_f) - abs(sum) > 0.00001 do begin
+        sum := sum + x**i/i;
+        i := i + 1;
+        n := n + 1;
+        writeln('Результат функции: ', res_f, ' : ', -sum, '.');
+    end;
+end.
